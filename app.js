c49f88f6e159019738edc163ec0e877dab3c0b25
unknown <wolfpykun@gmail.com>
2023-12-01 14:36:52 +0600

    update

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const square = document.getElementById('colorSquare');
+  const square = document.getElementById('colorSquare')
 
   function generateRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
+    const letters = '0123456789ABCDEF'
+    let color = '#'
     for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+      color += letters[Math.floor(Math.random() * 16)]
     }
-    return color;
+    return color
   }
 
   function changeColor() {
-    const randomColor = generateRandomColor();
-    square.style.backgroundColor = randomColor;
-    square.innerText = randomColor;
+    const randomColor = generateRandomColor()
+    square.style.backgroundColor = randomColor
+    square.innerText = randomColor
   }
 
-  square.addEventListener('click', changeColor);
-});
+  square.addEventListener('click', changeColor)
+})
